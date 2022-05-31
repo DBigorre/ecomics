19e0b79ed7c427d6cdcc70e95c88da8db201a50c
desiree bigorre <bigorredesiree@gmail.com>
2022-05-31 21:19:33 +0200

    controller avant test local

---
+++ app/controllers/comics_controller.rb
@@ -0,0 +1,51 @@
+class ComicsController < ApplicationController
+  def new
+    @comic = Comic.new
+  end
+
+  def create
+    @comic = Comic.new(comic_params)
+    @comic.save
+
+    if @comic.save
+      redirect_to comics_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @comics = Comic.all
+    @dc_comics = Comic.where(house: 'DC Comics')
+    @marvel_comics = Comic.where(house: 'Marvel')
+    @valiant_comics = Comic.where(house: 'Valiant')
+    @other_comics = Comic.where(house: 'Other')
+  end
+
+  def show
+    @comic = Comic.find(params[:id])
+  end
+
+  def indexdc
+    @dc_comics = Comic.where(house: 'DC Comics')
+  end
+
+  def indexmarvel
+    @marvel_comics = Comic.where(house: 'Marvel')
+  end
+
+  def indexvaliant
+    @valiant_comics = Comic.where(house: 'Valiant')
+  end
+
+  def indexother
+    @other_comics = Comic.where(house: 'Other')
+  end
+
+  private
+
+  def comic_params
+    params.require(:comic).permit(:name, :illustration, :house, :serie, :collector, :price, :date)
+  end
+
+  end
